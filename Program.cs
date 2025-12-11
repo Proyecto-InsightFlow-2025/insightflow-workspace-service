e7d899274cbe66d7f3c6ab19cf797ddb02d9c6dd
Eltosergi <Sergio.parada@alumnos.ucn.cl>
2025-12-11 11:53:37 -0300

    Fix(render): fixed render problem

--- Program.cs
+++ Program.cs
@@ -2,6 +2,9 @@
 using insightflow_workspace_service.src.Data;
 using insightflow_workspace_service.src.Helpers;
 
+Environment.SetEnvironmentVariable("DOTNET_USE_POLLING_FILE_WATCHER", "true");
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSingleton<ApplicationDBContext>();
